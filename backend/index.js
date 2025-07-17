e35ddf433e381f6842663c95f816299986750a4e
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2025-07-17 12:36:25 +0200

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -188,10 +188,49 @@ app.post("/login", async (req, res) => {
     }
 });
 
-app.listen(port, (error) => {
-    if (!error) {
-        console.log("Server is running on port" + port);
-    } else {
-        console.log("Error:" + error);
+const fetchUser = async (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).json({errors:"Please authenticate using valid token"});
     }
+    else {
+        try {
+            const data = jwt.verify(token, 'secret_ecom');
+            req.user = data.user;
+            next();
+        } catch (error) {
+            res.status(401).json({errors:"please authenticate using a valid token"});
+        }
+    }
+};
+
+app.post('/addtocart', fetchUser, async (req, res) => {
+    console.log("Added", req.body.itemId);
+    let userData = await User.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] += 1;
+    await User.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Added");
+});
+
+app.post('/removefromcart', fetchUser, async (req, res) => {
+    console.log("removed", req.body.itemId);
+    let userData = await User.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -= 1;
+    await User.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Removed");
+});
+
+app.post('/getcart', fetchUser, async (req, res) => {
+    console.log("GetCart");
+    let userData = await User.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+});
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server is running on port" + port);
+  } else {
+    console.log("Error:" + error);
+  }
 });
